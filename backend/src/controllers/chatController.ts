5d3e0a80fe129cf2e1fba340b06438fda16e8353
kuslhhh <kushaljadhav7745@gmail.com>
2025-11-11 08:30:24 +0530

    chatRoutes

---
+++ backend/src/controllers/chatController.ts
@@ -0,0 +1,48 @@
+import type { Request, Response } from "express"
+import prisma from "../config/prisma";
+
+export const sendMessage = async (req: Request, res: Response) => {
+   try {
+      const { receiverId, content, hash } = req.body;
+
+      const senderId = req.user?.id;
+      if (!req.user || !senderId) {
+         return res.status(401).json({ message: "Unauthorized" })
+      }
+
+      if (!receiverId || !content || !hash) {
+         return res.status(400).json({ message: "Missing required fields" })
+      }
+
+      const msg = await prisma.message.create({
+         data: { receiverId, senderId, hash, content },
+         include: { sender: true, receiver: true }
+      })
+
+      res.status(201).json(msg)
+
+   } catch (err) {
+      res.status(401).json({ message: "Error while sending message" })
+   }
+}
+
+export const getChatHistory = async (req: Request, res: Response) => {
+   const userId = req.params;
+   const myId = req.user?.id;
+
+   try {
+      const messages = await prisma.message.findMany({
+         where: {
+            OR: [
+               { senderId: myId, receiverId: userId },
+               { senderId: userId, receiverId: myId }
+            ]
+         },
+         orderBy: { timestamp: "asc" }
+      });
+      res.json(messages)
+   } catch (err) {
+      console.error(err);
+      res.status(401).json({ message: "Error fetching messages" })
+   }
+}
